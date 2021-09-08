786c2fccee81265edae6fafc551ef20b1f732b53
Igor Oliveira <devigor.tere@gmail.com>
2021-09-08 12:16:59 -0300

    feat(index): finish the index
    The index page now make the request from api and return all data to be used

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,32 @@
-import Container from 'components/Container'
-import Logo from 'components/Logo'
+import { gql } from 'graphql-request'
+import { client } from 'graphql/client'
+import { GetStaticProps } from 'next'
+import Homepage, { HomeProps } from 'templates/Homepage'
 
-export default function Home() {
+export default function Home({ name, title }: HomeProps) {
   return (
     <>
-      <Container>
-        <Logo />
-      </Container>
+      <Homepage title={title} name={name} />
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const query = gql`
+    query getHomepage {
+      home(where: { id: "ckt5toc88a33c0b28t6fxffp8" }) {
+        name
+        title
+      }
+    }
+  `
+
+  const { home } = await client.request(query)
+
+  return {
+    props: {
+      title: home.title,
+      name: home.name
+    }
+  }
 }
