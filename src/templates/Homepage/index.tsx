9d512ae8562c3d12bfdc0a7e225dfd336841a5ad
Igor Oliveira <devigor.tere@gmail.com>
2021-09-09 10:59:17 -0300

    feat(homepage): finish the markup with mobile menu

--- src/templates/Homepage/index.tsx
+++ src/templates/Homepage/index.tsx
@@ -1,4 +1,5 @@
-import Link from 'next/link'
+import { GithubIcon, LinkedinIcon, TwitterIcon } from 'Components/Icons'
+import S from './styles.module.scss'
 
 export type HomeProps = {
   name: string
@@ -7,37 +8,49 @@ export type HomeProps = {
 
 export default function Homepage({ name, title }: HomeProps) {
   return (
-    <main>
-      <section>
-        <h1>{name}</h1>
-        <h1>{title}</h1>
-      </section>
-      <section>
-        <nav>
+    <main className={S.homeContainer}>
+      <section className={S.rightSide}>
+        <h1 className={S.homeTitle}>{name}</h1>
+        <h1 className={S.homeSubtitle}>{title}</h1>
+        <nav className={S.mobileMenu}>
           <ul>
-            <li>
-              <Link href="/about">about</Link>
+            <li className={S.menuItem}>
+              <a href="/about">about</a>
             </li>
-            <li>
-              <Link href="/projects">projects</Link>
+            <li className={S.menuItem}>
+              <a href="/projects">projects</a>
             </li>
-            <li>
-              <Link href="https://dev.to/devigor">blog</Link>
+            <li className={S.menuItem}>
+              <a href="https://dev.to/devigor">blog</a>
+            </li>
+          </ul>
+        </nav>
+      </section>
+
+      <section className={S.leftSide}>
+        <nav className={S.homeMenu}>
+          <ul>
+            <li className={S.menuItem}>
+              <a href="/about">about</a>
+            </li>
+            <li className={S.menuItem}>
+              <a href="/projects">projects</a>
+            </li>
+            <li className={S.menuItem}>
+              <a href="https://dev.to/devigor">blog</a>
             </li>
           </ul>
 
-          <div>
-            <ul>
-              <li>
-                <Link href="">Github</Link>
-              </li>
-              <li>
-                <Link href="">Linkedin</Link>
-              </li>
-              <li>
-                <Link href="">Twitter</Link>
-              </li>
-            </ul>
+          <div className={S.iconGroup}>
+            <a href="">
+              <GithubIcon />
+            </a>
+            <a href="">
+              <LinkedinIcon />
+            </a>
+            <a href="">
+              <TwitterIcon />
+            </a>
           </div>
         </nav>
       </section>
