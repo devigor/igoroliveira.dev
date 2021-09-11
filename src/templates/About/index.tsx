e6529c1a1a8a549b5f218682ba8fa8bf3676db0e
Igor Oliveira <devigor.tere@gmail.com>
2021-09-11 10:55:48 -0300

    feat(aboutpage): finish the initial style of about page

--- src/templates/About/index.tsx
+++ src/templates/About/index.tsx
@@ -1,15 +1,16 @@
+import S from './styles.module.scss'
+
 export type AboutProps = {
   photo: string
   description: string
 }
 
 export default function Aboutpage({ photo, description }: AboutProps) {
-  console.log(photo, '\n', description)
   return (
-    <main className="">
-      <div className="">
+    <main className={S.aboutContainer}>
+      <div className={S.infoContainer}>
         <img src={photo} alt="Homem de óculos em fundo amarelo com sorriso de canto de rosto" />
-        <div dangerouslySetInnerHTML={{ __html: description }} />
+        <div className={S.aboutText} dangerouslySetInnerHTML={{ __html: description }} />
       </div>
     </main>
   )
